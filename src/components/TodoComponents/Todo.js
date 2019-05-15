ec3ec5896109af09c182d2edbcbe003b79df3f4e
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-05-14 22:09:43 -0400

    imported TodoForm component to App.js, Set up framework of an array in Todo.js

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const tasks = [
+    {
+        task: "Workout",
+        id: ,
+        completed: false
+    },
+    {
+        task: "Fix dinner",
+        id: ,
+        completed: false
+    },
+    {
+        task: "Look over Training Kit",
+        id: ,
+        completed: false
+    },
+    {
+        task: "Attend Lecture",
+        id: ,
+        completed: false
+    },
+    {
+        task: "Begin Project"
+        id: ,
+        completed: false
+    }
+];
+
+export default Todo;
